123c4c16fece72e7ddfae2a734667f9dc864b391
MikePia <lynnpete11@gmail.com>
2021-02-20 14:00:04 -0700

    Added report for candles

--- models/candlesmodel.py
+++ models/candlesmodel.py
@@ -8,6 +8,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import text
 
 from stockdata.dbconnection import getSaConn
+from stockdata.sp500 import sp500symbols, nasdaq100symbols
 from utils.util import dt2unix, unix2date
 
 Base = declarative_base()
@@ -84,17 +85,22 @@ class CandlesModel(Base):
         s = Session(bind=engine)
         if tickers == None:
             tickers = s.query(distinct(CandlesModel.symbol)).all()
+            tickers = [x[0] for x in tickers]
         # There is probably some cool way to get all the data in one sql statement. THIS COULD BE VERY TIME CONSUMING
-        for tick in tickers:
+        for tick in tickers[::-1]:
             # I think first thing is just change this to a sql execute without ORM (did not help much)
             # select min(time), max(time), count(time) from candles where symbol="SIRI";
           
             with engine.connect() as con:    
-                statement = text(f"""SELECT min(time), max(time), count(time) FROM candles WHERE symbol="{tick[0]}";""")
+                statement = text(f"""SELECT min(time), max(time), count(time) FROM candles WHERE symbol="{tick}";""")
                 q = con.execute(statement).fetchall()
-                # q = s.query(func.min(CandlesModel.time), func.max(CandlesModel.time), func.count(CandlesModel.time)).filter_by(symbol=tick[0]).all()
-                d[tick[0]] = [q[0][0], q[0][1], q[0][2]]
-                print(f'{tick[0]}: {unix2date(q[0][0])}: {unix2date(q[0][1])}: {q[0][2]} ')
+                # q = s.query(func.min(CandlesModel.time), func.max(CandlesModel.time), func.count(CandlesModel.time)).filter_by(symbol=tick).all()
+                if q[0][2] == 0:
+                    d[tick] = [q[0][0], q[0][1], q[0][2]]
+                    print(f'{tick}: {q[0][0]}: {q[0][1]}: {q[0][2]} ')
+                else:
+                    d[tick] = [unix2date(q[0][0]), unix2date(q[0][1]), q[0][2]]
+                    print(f'{tick}: {unix2date(q[0][0])}: {unix2date(q[0][1])}: {q[0][2]} ')
         return d
 
 
@@ -116,15 +122,39 @@ class ManageCandles:
         """
         Could analyze differences in db holdings here, For now just print it out
         """
+        from stockdata.dbconnection import getCsvDirectory
+        import csv
         d = CandlesModel.getReport(self.engine, tickers)
-        for k, v, in d.items():
-            print(f'{k}: {unix2date(v[0])}: {unix2date(v[1])}: {v[2]} ')
+
+
+
+        fn = getCsvDirectory() + "/report.csv"
+        with open(fn, 'a', newline='') as file:
+            for k, v, in d.items():
+                csv_file = csv.writer(file)
+                csv_file.writerow([k, v[0], v[1], v[2]])
+                # print(f'{tick}: {unix2date(q[0][0])}: {unix2date(q[0][1])}: {q[0][2]} ')
+                print(f'{k}: {v[0]}: {v[1]}: {v[2]} ')
+
+
+
+
+
+
+
+
+
+    def getQ100_Sp500(self):
+        st = set(sp500symbols).union(set(nasdaq100symbols))
+        st = sorted(list(st))
+        return st
+
 
 
 if __name__ == '__main__':
     mc = ManageCandles(getSaConn())
     tickers = ['TXN', 'SNPS', 'SPLK', 'PTON', 'CMCSA', 'GOOGL']
-    mc.reportShape(tickers=tickers)
+    mc.reportShape(tickers=mc.getQ100_Sp500()[:10])
     # # Create a classa or method to house jsoninfy Sqlalchemy results
     # import datetime as dt
     # import json
