a8cb83d0f66616078cbefb9b399aad621f076cdc
MikePia <lynnpete11@gmail.com>
2021-03-21 16:13:02 -0600

    local install stuff

---
+++ scripts/env.py
@@ -0,0 +1,23 @@
+
+import os
+
+
+def getdbname():
+    """
+    Explanation
+    -----------
+    Establish the location of the sqlite keys database.
+    The contents of the database contains keys and passwords and  must be populated seperatly.
+    """
+    fn = os.path.join(os.getcwd())
+    fn = os.path.join(fn, '..')
+    fn = os.path.normpath(fn)
+    fn = os.path.join(fn, 'keys.sqlite')
+    return f'sqlite:///{fn}'
+
+
+sqlitedb = getdbname()
+
+
+if __name__ == '__main__':
+    print(sqlitedb)
