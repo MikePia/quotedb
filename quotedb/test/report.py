f0e189fa22ad159feb1f67de998a227d460f6871
MikePia <lynnpete11@gmail.com>
2021-04-21 11:07:05 -0600

    report

---
+++ quotedb/test/report.py
@@ -0,0 +1,32 @@
+import time
+from quotedb.utils import util
+
+from quotedb import sp500
+from quotedb import getdata as gd
+from quotedb.models import metamod as mm
+
+
+# stocks = ['CERN', 'CSCO', 'GILD', 'KDP', 'MAR', 'MU', 'AAPL']
+def runit():
+    for i in range(5, 75, 5):
+        stocks = sp500.random50(numstocks=i)
+        stocks.append('BINANCE:BTCUSDT')
+        # delt = dt.timedelta(seconds=0.25)
+        fn = f"__{i+1}_reportKA_fill_250.json"
+        fn = util.formatFn(fn, 'json')
+
+        # fq = util.dt2unix_ny(dt.datetime(2021, 4, 19, 9, 30))
+        # gd.startTickWS_SampleFill(stocks, fn, fq, delt=delt)
+        store = ['json']
+        gd.startTickWSKeepAlive(stocks, fn, store, delt=None,)
+        mm.cleanup()
+        mm.init()
+        print("sleeping for 30")
+        time.sleep(30)
+
+
+runit()
+
+
+# begin = time.perf_counter()
+# gd.startTickWSKeepAlive(stocks, fn, store=['json'], delt=None, polltime=5)
